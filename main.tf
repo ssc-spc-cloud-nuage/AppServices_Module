f6d3f45638e55500bb1c11f060474bef151eafa0
Louis-Eric Tremblay <louis-eric.tremblay@canada.ca>
2021-04-07 15:19:56 -0400

    commit

--- main.tf
+++ main.tf
@@ -1,27 +1,43 @@
-resource "azurerm_app_service_plan" "CIO-VCBOARDROOM-asp" {
-  name                = "${local.environment}-cio-vcboardroom-asp"
-  location            = local.resource_groups_L3.AppServices.location
-  resource_group_name = local.resource_groups_L3.AppServices.name
-
-  sku {
-    tier = "PremiumV2"
-    size = "P1v2"
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 2.52.0"
+    }
   }
 }
 
-resource  "azurerm_app_service" "CIO-VCBOARDROOM-aps" {
-  name                = "${local.environment}ciovcboardroom"
-  location            = local.resource_groups_L3.AppServices.location
-  resource_group_name = local.resource_groups_L3.AppServices.name
-  app_service_plan_id = azurerm_app_service_plan.CIO-VCBOARDROOM-asp.id
+# locals {
+#     deploydbs = {
+#     for x in var.server.SQL_Database : 
+#       "${x.sqldbname}" => x if lookup(x, "deploy", true) != false
+#   }
+# }
+
+resource "azurerm_app_service_plan" "appservices-asp" {
+  name                ="${var.environment}-cio-${var.AppServicesPlan["name"]}"
+  location            = var.location
+  resource_group_name = var.AppServicesPlan["resource_group_name"]
+
+  sku {
+    tier = var.AppServicesPlan["tier"] # "PremiumV2"
+    size = var.AppServicesPlan["size"] # "P1v2"
+  }
+}
+
+resource  "azurerm_app_service" "appservices-aps" {
+  name                = "${var.environment}-cio-${var.AppServices["name"]}"
+  location            = var.location
+  resource_group_name = var.AppServices["resource_group_name"]
+  app_service_plan_id = azurerm_app_service_plan.appservices-asp.id
   
   app_settings = {
-    "WEBSITE_DNS_SERVER" = "168.63.129.16",
-    "WEBSITE_VNET_ROUTE_ALL" = "1"
+    "WEBSITE_DNS_SERVER" = var.AppServices["WEBSITE_DNS_SERVER"] # "168.63.129.16",
+    "WEBSITE_VNET_ROUTE_ALL" = var.AppServices["WEBSITE_VNET_ROUTE_ALL"] # "1"
   }
 }
 
 resource "azurerm_app_service_virtual_network_swift_connection" "vnetintegrationconnection" {
-  app_service_id  = azurerm_app_service.CIO-VCBOARDROOM-aps.id
+  app_service_id  = azurerm_app_service.appservices-aps.id
   subnet_id       = local.subnets.APP.id
 }
